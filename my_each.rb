f5d6096244e32d58ce6114bd11717d9916f3e917
Roberto Samia <bojosteph@gmail.com>
2018-10-28 00:19:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(words)
   # code here
-end
+  
+  counter = 0
+  while counter < words.size
+    yield(words[counter])
+    counter += 1
+  end
+  words
+ end
+ 
+ 
